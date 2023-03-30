0cf871b9aec8b18207e34be6ec7c1d297e3c6c8b
Abbiraam Sinnathamby <169357@student.horizoncollege.nl>
2023-03-30 15:17:35 +0200

    highlight werkt alleen tekst in vs

--- projectopen.php
+++ projectopen.php
@@ -158,9 +158,96 @@ $_SESSION['welk'] = $welk;
         <option value="vs">vs</option>
     </select>
     <pre id="code">
-    function hello() {
-      console.log("Hello, world!");
-    }
+    -- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 24 mrt 2023 om 09:44
+-- Serverversie: 10.4.27-MariaDB
+-- PHP-versie: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `codegarden`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `private`
+--
+
+CREATE TABLE `private` (
+  `id` MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `Title` varchar(30) NOT NULL,
+  `Description` varchar(300) NOT NULL,
+  `Language` varchar(30) NOT NULL,
+  `Code` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `public`
+--
+
+CREATE TABLE `public` (
+  `id` MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `Title` varchar(30) NOT NULL,
+  `Description` varchar(300) NOT NULL,
+  `Language` varchar(30) NOT NULL,
+  `Code` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` mediumint(9) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
 </pre>
               <?php
 
