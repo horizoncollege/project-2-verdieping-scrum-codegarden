d7c8bd598c1c9cdce9a0e88b64d5f085ddca363e
Abbiraam Sinnathamby <169357@student.horizoncollege.nl>
2023-03-31 09:48:17 +0200

    highlighter wertrkkkkkkllk

--- projectopen.php
+++ projectopen.php
@@ -13,16 +13,16 @@
     <link rel="stylesheet" href="style.css">
     <title>Edit</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/highlight.min.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/default.min.css"
-        id="theme">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/default.min.css" id="theme">
     <script>
-        window.onload = function () {
-            var code = document.getElementById("code");
-            hljs.highlightBlock(code);
-        };
-        function updateTheme(theme) {
-            document.getElementById("theme").href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/" + theme + ".min.css";
-        }
+      window.onload = function() {
+        var code = document.getElementById("code");
+        hljs.highlightBlock(code);
+      };
+
+      function updateTheme(theme) {
+        document.getElementById("theme").href = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.0.3/styles/" + theme + ".min.css";
+      }
     </script>
   </head>
 
@@ -130,7 +130,7 @@
   }
 
   $welk = $_GET['id'];
-$_SESSION['welk'] = $welk;
+  $_SESSION['welk'] = $welk;
 
   $sql = "SELECT * FROM private WHERE id='$welk'";
 
@@ -150,116 +150,29 @@ $_SESSION['welk'] = $welk;
 
             <div class="yourding">
 
-            <select onchange="updateTheme(this.value)">
-        <option value="default">default</option>
-        <option value="a11y-dark">a11y-dark</option>
-        <option value="atom-one-dark">atom-one-dark</option>
-        <option value="rainbow">rainbow</option>
-        <option value="vs">vs</option>
-    </select>
-    <pre id="code">
-    -- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Gegenereerd op: 24 mrt 2023 om 09:44
--- Serverversie: 10.4.27-MariaDB
--- PHP-versie: 8.2.0
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `codegarden`
---
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `private`
---
-
-CREATE TABLE `private` (
-  `id` MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Title` varchar(30) NOT NULL,
-  `Description` varchar(300) NOT NULL,
-  `Language` varchar(30) NOT NULL,
-  `Code` longblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `public`
---
-
-CREATE TABLE `public` (
-  `id` MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Title` varchar(30) NOT NULL,
-  `Description` varchar(300) NOT NULL,
-  `Language` varchar(30) NOT NULL,
-  `Code` longblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Tabelstructuur voor tabel `users`
---
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
-  `id` mediumint(9) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexen voor geëxporteerde tabellen
---
-
---
--- Indexen voor tabel `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT voor geëxporteerde tabellen
---
-
---
--- AUTO_INCREMENT voor een tabel `users`
---
-ALTER TABLE `users`
-  MODIFY `id` mediumint(9) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-</pre>
               <?php
 
-              // echo "<input id='Title' name='Title' class='Searchbar' type='text' maxlength='30' placeholder='$title' required></input>";
               foreach ($conn->query($sql) as $row) {
                 $welk = $row['id'];
 
                 echo "<input id='replacetitle' name='replacetitle' class='Searchbar' placeholder='" . $row['Title'] . "' required></input<br><br>";
                 echo "<input id='replacelanguage' name='replacelanguage' class='Searchbar' placeholder='" . $row['Language'] . "'required></input<br><br>";
                 echo "<p>Description: </p><textarea id='replacedescription' name='replacedescription' class='Searchbar' style='width: 400px; height: 150px; resize:vertical; max-height:750px; min-height:150px;'>" . $row['Description'] . "</textarea>";
-                echo "<textarea id='replacecode' name='replacecode' class='form-control flex-grow-1' style='background-color:#282a3a; color:white; max-width:75%; display:flex; justify-content:center; align-items:center;' spellcheck='false' cols='100' rows='15'> " . $row['Code'] . "</textarea>";
+               
+              ?>
+                <select onchange="updateTheme(this.value)">
+                  <option value="default">default</option>
+                  <option value="a11y-dark">a11y-dark</option>
+                  <option value="atom-one-dark">atom-one-dark</option>
+                  <option value="rainbow">rainbow</option>
+                  <option value="vs">vs</option>
+                </select>
 
+              <?php
+                echo "<pre id='code'>";
+                echo $row['Code'];
+                echo "</pre>";
                 echo "<br><button>
                   <div class='svg-wrapper-1'>
                     <div class='svg-wrapper'>
